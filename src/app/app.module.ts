689a18acedc23ee1d581b28fd129804223d25241
Joel Mavinga-Fernandes <joelfernandes35@hotmail.com>
2021-08-09 23:24:51 -0300

    only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraCimaComponent } from './barra-cima/barra-cima.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraCimaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/barra-cima/barra-cima.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-cima',
+  templateUrl: './barra-cima.component.html',
+  styleUrls: ['./barra-cima.component.css']
+})
+export class BarraCimaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
