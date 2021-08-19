78c91d929d04d2bb0b05d9e4fe9303c0571adb9a
Joel Mavinga-Fernandes <joelfernandes35@hotmail.com>
2021-08-19 17:19:28 -0300

    alterações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarraCimaComponent } from './barra-cima/barra-cima.component';
-import { HomeComponent } from './home/home.component';
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BarraCimaComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/Formulario.model.ts
@@ -0,0 +1,161 @@
+import { SafeResourceUrl } from '@angular/platform-browser';
+
+export class Formulario{
+    public id: number
+    public nomeSubestacao: string
+    public local: string
+    public email: string
+    public dataFiscalizacao: string
+    public tipoEstacao: string
+    public nivelTensao: string
+    public potenciaTransfQdeEntLT: string
+    public potenciaTransfQdeSaiLT: string
+    public potenciaTransfNivTensEntrLT: string
+    public potenciaTransfNivTensSaiLT: string
+    public verificacaoAtualizacaoDiagramaUnifiliar: string
+    public descricaoVerificacao: string
+    public equipNivelCurtCircuit: string
+    public equipNivelIsolamento: string
+    public equipVidaUtil: string
+    public equipTempoInstalacao: string
+    public paraRaiosTempConexEletrica: string
+    public paraRaiosMaterial: string
+    public bobinasBloqueioTempConexEletrica: string
+    public bobinBloqueioCondiConservaCxaComunicaC: string
+    public TPsTemperatConexElect: string
+    public TPsTipo: string
+    public TCsTemperatConexElect: string
+    public TCsTipo: string
+    public chavesTemperatConexElect: string
+    public chavesTipoAberturA: string
+    public chavesTipo: string
+    public chavesCxa: string
+    public chavesTensNominal: string
+    public chavesCorrenteNominal: string
+    public disjuntoresTemperatConexElect: string
+    public disjuntoresMecanismo: string
+    public disjuntoresPainel: string
+    public disjuntorSistemaDisparador: string
+    public trfo1Tipo: string
+    public trfo1Ausencia: string
+    public trfo1Verificacao: string
+    public trfo1Saturacao: string
+    public trfo1VerificacaoTemperatura: string
+    public trfo1NivelRuído: string
+    public trfo1PotenciaNominal: string
+    public trfo1TipoIsolação: string
+    public trfo1EstadoConservação: string
+    public trfo1AusenciaPontos: string
+    public trfo1PlacaInformações: string
+    public trfo1FechamentoEnrolamentos: string
+    public trfo1ExistnciaComutador: string
+    public trf2Impedancia: string
+    public trf2AusenciaVazamento: string
+    public trf2Saturacao: string
+    public trf2VerificacaoTemperatura: string
+    public trfo2NivelRuído: string
+    public trfo2PotenciaNominal: string
+    public trfo2TipoIsolação: string
+    public trfo2EstadoConservação: string
+    public trfo2AusenciaPontos: string
+    public trfo2PlacaInformações: string
+    public trfo2FechamentoEnrolamentos: string
+    public trfo2ExistnciaComutador: string
+    public bancoCapacitoresNivelTensao: string
+    public bancoCapacitoresPotnciaCelula: string
+    public bancoCapacitoresPotenciaBanco: string
+    public bancoCapacitoresTipoMaterIsolante: string
+    public TipoLigBancoCapacitores: string
+    public reatores: string
+    public tipoDiscoQtdIsoladores: string
+    public tipoDiscoMaterial: string
+    public tipoColunaQtdIsoladores: string
+    public tipoColunaMaterial: string
+    public muflas: string
+    public cubiculos: string
+    public paineisComandControle: string
+    public paineisProtec: string
+    public paineisSoftware: string
+    public paineisComunicacProtec: string
+    public paineisComunicaRemota: string
+    public paineisEstadoConserva: string
+    public paineisConexAdequada: string
+    public equipamentosServicosAuxTransformadores: string
+    public equipamentosServicosAuxPainel: string
+    public equipamentosServicosAuxSalaConjunto: string
+    public equipamentosServicosAuxLavaolhos: string
+    public equipamentosServicosAuxVentilacao: string
+    public equipamentosServicosAuxCarregador: string
+    public equipamentosServicosAuxAusencia: string
+    public equipamentosServicosAuxGerador: string
+    public cablagemVerificacaoConservacao: string
+    public cablagemVerificacaoSuficiente: string
+    public aterramentoProtecaoConntraDescargasAtmosfericas: string
+    public arranjoEletromecanico: string
+    public arranjoEletromecanicoTipoManobra: string
+    public arranjoEletromecanicoVL1: string
+    public arranjoEletromecanicoVL2: string
+    public arranjoEletromecanicoVerificarCondicoes: string
+    public arranjoEletromecanicoPintura: string
+    public arranjoEletromecanicoAusenciaCorrosao: string
+    public arranjoEletromecanicoETD: string
+    public arranjoEletromecanicoRelatos: string
+    public arranjoEletromecanicoVerificacaoConexoseltricas: string
+    public arranjoEletromecanicoQtdPontos: string
+    public arranjoEletromecanicoMaiortemperatura: string
+    public arranjoEletromecanicoVerificarEstado: string
+    public arranjoEletromecanicoDisponibilidade: string
+    public arjoEletromecanicoVerificEstadConserva: string
+    public arranjoEletromecanicoVerifOcorrencia: string
+    public arranjoEletromecanicoAusenciaTampa: string
+    public arranjoEletromecanicoIstadoConserva: string
+    public arranjoEletroVerifAdequacao: string
+    public arranjoEletroVerificDistancias: string
+    public arranjoEletroExistencia: string
+    public arranjoEletroCaixa: string
+    public testFuncionamento: string
+    public testManobra: string
+    public testManobraDisjuntores: string
+    public testPartida: string
+    public testComissionamento: string
+    public testRel: string
+    public testRuido: string
+    public testTemperatura: string
+    public consInfraMuro: string
+    public consInfraConcertina: string
+    public consInfraPlacaETD: string
+    public consInfraPlacasinalizacao: string
+    public consInfraEntorno: string
+    public consInfraAterramento: string
+    public consInfraValidade: string
+    public consInfraDisponibilidade: string
+    public consInfraAusencia: string
+    public consInfraFuncionalidade: string
+    public consInfraAdequacaoAcomodacoes: string
+    public consInfraExistenciaEPCs: string
+    public consInfraPintura: string
+    public consInfraAusenciaMaFormac: string
+    public consInfraAusnciaVegetação: string
+    public consInfraMonitoramento: string
+    public consInfraNivelRuido: string
+    public consInfraAusnciaEquipament: string
+    public consInfraEquipamentOperando: string
+    
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+}
